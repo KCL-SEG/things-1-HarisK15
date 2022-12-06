bff4ec79ce347cbd70661efd47db481345af1fcc
haris kamran <harisk543@hotmail.com>
2022-12-06 00:30:06 +0000

    user model thing added

--- things/models.py
+++ things/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.db.models import Model
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class Thing(AbstractUser):
+    bio = models.TextField()
+    quantity = models.PositiveIntegerField()
+    description = models.TextField()
