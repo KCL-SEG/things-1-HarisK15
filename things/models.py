c6ffce5dd99b8841792fd264e671bf7bb4aac7ec
haris kamran <harisk543@hotmail.com>
2023-03-12 00:48:33 +0000

    fix

--- things/models.py
+++ things/models.py
@@ -1,8 +1,12 @@
+
+
 from django.db import models
+from django.core.validators import RegexValidator
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
-
-class Thing (models.Model):
-    name = models.CharField()
-    description = models.TextField()
-    quantity = models.IntegerField()
+class Thing(models.Model):
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, unique=False, blank=True)
+    quantity = models.IntegerField(validators=[MinValueValidator(limit_value=0), MaxValueValidator(limit_value=100)])
